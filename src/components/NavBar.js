064f7425189c065d66f51da480339069e4077102
ldv432 <ldv432@gmail.com>
2024-03-01 22:15:04 -0700

    KEYS AND LISTS

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,10 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
-
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const linkies = links.map((link, index) =>{
+    return <a key={index} href={`#${link}`}>{link}</a>
+  })
+    return <nav>{linkies}</nav>;
 }
 
 export default NavBar;
--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,13 +1,15 @@
 import React from "react";
 
+
 function ProjectItem({ name, about, technologies }) {
+
+  const techies = technologies.map((techs, index)=> <span key={index}>{techs}</span>)
+  
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
-      <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
-      </div>
+      <div className="technologies"> {techies} </div>
     </div>
   );
 }
